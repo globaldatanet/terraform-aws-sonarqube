3df2d2aacbe979de93e098f91da22bc7905311db
Mohammed Yahya <my.almusaddar@gmail.com>
2020-07-17 10:30:19 +0300

    use rds instead of aurora

--- database.tf
+++ database.tf
@@ -18,51 +18,37 @@ resource "aws_db_subnet_group" "aurora_db_subnet_group" {
 #------------------------------------------------------------------------------
 # AWS RDS Aurora Cluster
 #------------------------------------------------------------------------------
-resource "aws_rds_cluster" "aurora_db" {
+resource "aws_db_instance" "this" {
   depends_on = [aws_kms_key.encryption_key]
-
-  # Cluster
-  cluster_identifier     = "${var.name_preffix}-sonar-aurora-db"
-  vpc_security_group_ids = [aws_security_group.aurora_sg.id]
-  db_subnet_group_name   = aws_db_subnet_group.aurora_db_subnet_group.id
 
   # Encryption
   storage_encrypted = true
   kms_key_id        = aws_kms_key.encryption_key.arn
 
-  # Logs
-  #enabled_cloudwatch_logs_exports = ["audit", "error", "general"]
-  # Database
-  engine          = "aurora-postgresql"
-  engine_version  = local.sonar_postgres_sql_db_version
-  database_name   = local.sonar_db_name
-  master_username = local.sonar_db_username
-  master_password = local.sonar_db_password
+  # Networking
+  vpc_security_group_ids = [aws_security_group.aurora_sg.id]
+  db_subnet_group_name   = aws_db_subnet_group.aurora_db_subnet_group.id
+  publicly_accessible    = false
+
+  allocated_storage     = 50
+  max_allocated_storage = 100
+  storage_type          = "gp2"
+  engine                = "postgres"
+  instance_class        = local.sonar_db_instance_size
+  engine_version        = local.sonar_postgres_sql_db_version
+  name                  = local.sonar_db_name
+  username              = local.sonar_db_username
+  password              = local.sonar_db_password
 
   # Backups
   backup_retention_period = 3
-  preferred_backup_window = "07:00-09:00"
+  backup_window           = "07:00-09:00"
   skip_final_snapshot     = true
-  tags = {
-    Name = "${var.name_preffix}-sonar-aurora-db"
-  }
-}
 
-#------------------------------------------------------------------------------
-# AWS RDS Aurora Cluster Instances
-#------------------------------------------------------------------------------
-resource "aws_rds_cluster_instance" "aurora_db_cluster_instances" {
-  count                = length(var.availability_zones)
-  identifier           = "aurora-db-instance-${count.index}"
-  cluster_identifier   = aws_rds_cluster.aurora_db.id
-  db_subnet_group_name = aws_db_subnet_group.aurora_db_subnet_group.id
-  engine               = "aurora-postgresql"
-  engine_version       = local.sonar_postgres_sql_db_version
-  instance_class       = local.sonar_db_instance_size
-  publicly_accessible  = true
   tags = {
-    Name = "${var.name_preffix}-sonar-aurora-db-cluster-instances-${count.index}"
+    Name = "${var.name_preffix}-sonar-db"
   }
+
 }
 
 #------------------------------------------------------------------------------
@@ -83,7 +69,7 @@ resource "aws_security_group" "aurora_sg" {
     # If the expression in the following list itself returns a list, remove the
     # brackets to avoid interpretation as a list of lists. If the expression
     # returns a single list item then leave it as-is and remove this TODO comment.
-    security_groups = [ module.ecs_fargate.ecs_tasks_sg_id ]
+    security_groups = [module.ecs_fargate.ecs_tasks_sg_id]
   }
   egress {
     protocol    = -1
